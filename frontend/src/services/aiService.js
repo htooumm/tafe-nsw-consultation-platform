e80599b5c6fd98e8b6d77023d1de4c99d545ec4e
whereisalidev <786syedalihussain@gmail.com>
2025-08-30 17:02:05 +0500

    updated

--- frontend/src/services/aiService.js
+++ frontend/src/services/aiService.js
@@ -2,7 +2,9 @@ import OpenAI from 'openai';
 
 let openai;
 // const BACKEND_URL = "http://127.0.0.1:8004";
-const BACKEND_URL = "https://tafe-nsw-consultation-platform-backend.onrender.com";
+// const BACKEND_URL = "https://tafe-nsw-consultation-platform-backend.onrender.com";
+const BACKEND_URL = "https://tafe-nsw-consultation-platform-production.up.railway.app";
+
 
 export class AIService {
   async sendPriorityDiscoveryMessage(userMessage, context) { // Add context parameter
